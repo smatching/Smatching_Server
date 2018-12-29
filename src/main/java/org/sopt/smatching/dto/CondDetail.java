9446ebb0ad1aa703b7af45d241178222261c9e03
ktyu <yu_kitae@naver.com>
2018-12-29 22:16:06 +0900

    Age 컬럼도 BIT(4) 로 타입변경

--- src/main/java/org/sopt/smatching/dto/CondDetail.java
+++ src/main/java/org/sopt/smatching/dto/CondDetail.java
@@ -3,8 +3,7 @@ package org.sopt.smatching.dto;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.sopt.smatching.utils.Age;
-import org.sopt.smatching.utils.MutilpleOption;
+import org.sopt.smatching.utils.MultipleOption;
 
 import java.util.HashMap;
 
@@ -16,48 +15,48 @@ public class CondDetail {
     private int condIdx;
     private String condName;
     private boolean alert;
+
     private HashMap<String, Boolean> location;
-    private Age age; // 단일선택 옵션이라 enum 타입으로 정의
+    private HashMap<String, Boolean> age;
     private HashMap<String, Boolean> period;
-    private HashMap<String, Boolean> category;
     private HashMap<String, Boolean> field;
     private HashMap<String, Boolean> advantage;
     private HashMap<String, Boolean> busiType;
 
+    private HashMap<String, Boolean> excCategory;
 
-    // Cond -> CondDetail 생성자
+
+    // CondDetail 생성자 (Cond -> CondDetail)
     public CondDetail(Cond cond) {
 
         this.condIdx = cond.getCondIdx();
         this.condName = cond.getCondName();
         this.alert = cond.isAlert();
 
-        this.location = bitToMap(cond.getLocation(), MutilpleOption.LOCATIONS);
-        this.age = cond.getAge(); // ### enum 이용과정 코드에 문제없나 확인 필요
-        this.period = bitToMap(cond.getPeriod(), MutilpleOption.PERIODS);
-        this.category = bitToMap(cond.getCategory(), MutilpleOption.CATEGORYS);
-        this.field = bitToMap(cond.getField(), MutilpleOption.FIELDS);
-        this.advantage = bitToMap(cond.getAdvantage(), MutilpleOption.ADVANTAGES);
-        this.busiType = bitToMap(cond.getBusiType(), MutilpleOption.BUSITYPES);
+        this.location = bitToMap(cond.getLocation(), MultipleOption.LOCATIONS);
+        this.age = bitToMap(cond.getAge(), MultipleOption.AGES);
+        this.period = bitToMap(cond.getPeriod(), MultipleOption.PERIODS);
+        this.field = bitToMap(cond.getField(), MultipleOption.FIELDS);
+        this.advantage = bitToMap(cond.getAdvantage(), MultipleOption.ADVANTAGES);
+        this.busiType = bitToMap(cond.getBusiType(), MultipleOption.BUSITYPES);
+
+        // 사용자 입장 에선 Exclude Category(필요없는 지원분야)이므로 비트플립 해서 넘김
+        int invalidBitCnt = 32 - MultipleOption.CATEGORYS.length;
+        int flipped_category = ((~(cond.getCategory()) << invalidBitCnt) >> invalidBitCnt);
+        this.excCategory = bitToMap(flipped_category, MultipleOption.CATEGORYS);
     }
 
 
 
     public static HashMap<String, Boolean> bitToMap(long binary, String[] arr) {
         HashMap<String, Boolean> map = new HashMap<>();
-
-        if((binary&1) == 1) { // 최하위 비트가 1이면, 모든 객관식 보기를 전부 true로 설정
-            for (String key : arr)
-                map.put(key, true);
-        }
-        else { // // 최하위 비트가 1이 아니면, 그 다음비트부터 의미를 가진 모든 비트를 시프트연산 하면서 검사해서 t/f 설정
-            for (int i = 0; i < arr.length; i++) {
-                binary >>= 1;
-                map.put(arr[i], (binary&1) == 1);
-            }
+        for (int i = 0; i < arr.length; i++) { // 의미를 가진 모든 비트를 시프트연산 하면서 검사해서 t/f 설정
+            map.put(arr[i], (binary&1) == 1);
+            binary >>= 1;
         }
         return map;
     }
+
     public static HashMap<String, Boolean> bitToMap(int binary, String[] arr) {
         return bitToMap((long)binary, arr);
     }
