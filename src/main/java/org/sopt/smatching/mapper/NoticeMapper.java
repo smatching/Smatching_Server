50e2e4436d43a9594105cd1e669eb54339295c90
ktyu <yu_kitae@naver.com>
2019-01-02 03:43:38 +0900

    버그 픽스 - 맞춤공고 관련 조회 기능에 동적쿼리 적용
    특정 조건에 옵션을 하나 선택하지 않은 경우 필터링 하지 않아야함.

--- src/main/java/org/sopt/smatching/mapper/NoticeMapper.java
+++ src/main/java/org/sopt/smatching/mapper/NoticeMapper.java
@@ -3,8 +3,10 @@ package org.sopt.smatching.mapper;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.SelectProvider;
 import org.sopt.smatching.dto.Cond;
 import org.sopt.smatching.dto.NoticeSummary;
+import org.sopt.smatching.utils.DynamicQuery;
 
 import java.util.List;
 
@@ -38,35 +40,14 @@ public interface NoticeMapper {
                                                       @Param("existNum") final int existNum,
                                                       @Param("userIdx") final int userIdx);
 
-    // 맞춤공고 개수 조회 - ### 동적 쿼리로 바꿔야함!!
-    @Select("SELECT COUNT(notice.noticeidx) " +
-            "FROM notice " +
-            "WHERE notice.valid = 1 " +
-            "AND notice.location & #{cond.location} > 0 " +
-            "AND notice.category & #{cond.category} > 0 " +
-            "AND notice.age & #{cond.age} > 0 " +
-            "AND notice.period & #{cond.period} > 0 " +
-            "AND notice.field & #{cond.field} > 0 " +
-            "AND notice.advantage & #{cond.advantage} > 0 " +
-            "AND notice.busitype & #{cond.busiType} > 0")
+    // 맞춤공고 개수 조회(동적쿼리 사용)
+    @SelectProvider(type = DynamicQuery.class, method = "countFitNotice")
     int countFitNotice(@Param("cond") final Cond cond);
 
 
-    // 맞춤공고 목록 조회 - ### 동적 쿼리로 바꿔야함!!
-    @Select("SELECT notice.noticeIdx, notice.title, notice.institution, notice.end_date-current_date as dday, scrap_notice.scrap, notice.readcnt " +
-            "FROM notice " +
-            "LEFT OUTER JOIN scrap_notice " +
-            "ON notice.noticeIdx = scrap_notice.noticeIdx AND scrap_notice.useridx = #{userIdx} " +
-            "WHERE notice.valid = 1 " +
-            "AND notice.location & #{cond.location} > 0 " +
-            "AND notice.category & #{cond.category} > 0 " +
-            "AND notice.age & #{cond.age} > 0 " +
-            "AND notice.period & #{cond.period} > 0 " +
-            "AND notice.field & #{cond.field} > 0 " +
-            "AND notice.advantage & #{cond.advantage} > 0 " +
-            "AND notice.busitype & #{cond.busiType} > 0 " +
-            "ORDER BY notice.noticeIdx DESC " +
-            "LIMIT #{existNum}, #{reqNum}")
+
+    // 맞춤공고 목록 조회(동적쿼리 사용)
+    @SelectProvider(type = DynamicQuery.class, method = "findFitNoticeSummaryWithScrap")
     List<NoticeSummary> findFitNoticeSummaryWithScrap(@Param("reqNum") final int reqNum,
                                                       @Param("existNum") final int existNum,
                                                       @Param("userIdx") final int userIdx,
