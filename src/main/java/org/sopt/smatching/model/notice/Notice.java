bc2790a52f317c229c30506a2e1bb453a8aacef6
ktyu <yu_kitae@naver.com>
2019-01-03 23:16:49 +0900

    필요없는 import 삭제

--- src/main/java/org/sopt/smatching/model/notice/Notice.java
+++ src/main/java/org/sopt/smatching/model/notice/Notice.java
@@ -4,7 +4,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.sopt.smatching.model.cond.Cond;
-import org.sopt.smatching.model.notice.NoticeInput;
 import org.sopt.smatching.utils.MultipleOption;
 
 @Getter
